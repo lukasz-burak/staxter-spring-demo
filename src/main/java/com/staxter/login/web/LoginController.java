39e6bad189892c935e9c32d14cbcf14732bfee42
Lukasz Burak <lukasz.burak@inspeerity.com>
2019-11-26 18:20:01 +0100

    Implement exception handler for LoginController.

--- src/main/java/com/staxter/login/web/LoginController.java
+++ src/main/java/com/staxter/login/web/LoginController.java
@@ -1,8 +1,13 @@
 package com.staxter.login.web;
 
 import com.staxter.login.LoginService;
+import com.staxter.login.WrongCredentialsException;
 import com.staxter.userrepository.UserDto;
 import com.staxter.userrepository.UserLoginDto;
+import com.staxter.web.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +19,9 @@ import javax.validation.Valid;
 @RequestMapping("/userservice/login")
 class LoginController {
 
+    private static final String WRONG_CREDENTIALS_ERROR_CODE = "WRONG_CREDENTIALS";
+    private static final String WRONG_CREDENTIALS_ERROR_DESCRIPTION = "Username and password do not match";
+
     private final LoginService loginService;
 
     LoginController(LoginService loginService) {
@@ -23,5 +31,16 @@ class LoginController {
     @PostMapping
     UserDto registerUser(@RequestBody @Valid UserLoginDto user) {
         return loginService.loginUser(user);
+    }
+
+    @ExceptionHandler({WrongCredentialsException.class})
+    ResponseEntity<ErrorResponse> handleWrongCredentialsException() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                .body(
+                        new ErrorResponse(
+                                WRONG_CREDENTIALS_ERROR_CODE,
+                                WRONG_CREDENTIALS_ERROR_DESCRIPTION
+                        )
+                );
     }
 }
